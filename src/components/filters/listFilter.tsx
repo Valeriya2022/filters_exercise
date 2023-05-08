ff1b1ce0bc9a7f4a72f0a2521e3825d4c8036f18
nikavella <nikavella22@gmail.com>
2023-05-09 02:24:22 +0600

    added Searchbox to list filter

--- src/components/filters/listFilter.tsx
+++ src/components/filters/listFilter.tsx
@@ -1,10 +1,23 @@
 import { FilterListType } from '../../types/filter';
-import { Card, Checkbox } from 'antd';
+import { Card, Checkbox, Input, Space } from 'antd';
+import React, { useState } from 'react';
 
 export function ListFilter({ filter }: { filter: FilterListType }) {
+  const [filterList, setFilterList] = useState(filter.list_variants);
+
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) setFilterList(filter.list_variants);
+    const newFilterList = filter.list_variants.filter((variant) =>
+      variant.display_name.toLowerCase().startsWith(e.target.value.toLowerCase()),
+    );
+    setFilterList(newFilterList);
+  };
   return (
     <Card bordered={false} type='inner' title={filter.display_name}>
-      {filter.list_variants.map((variant) => {
+      <Space style={{ marginBottom: 10 }}>
+        <Input placeholder='search' onChange={onSearch} />
+      </Space>
+      {filterList.map((variant) => {
         return (
           <div key={variant.unique_id}>
             <Checkbox value={variant.unique_id}>{variant.display_name}</Checkbox>
